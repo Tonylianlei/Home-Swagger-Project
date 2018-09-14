b141eeebe8fc5922842eda102cc68fcf15cf2ddf
Tony <tony.lian@chinaventure.com.cn>
2018-09-14 15:48:52 +0800

    设置一个默认的请求参数

--- src/main/java/com/example/demo/configPool/swagger/SwaggerBase.java
+++ src/main/java/com/example/demo/configPool/swagger/SwaggerBase.java
@@ -10,6 +10,13 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * 创建人:连磊
@@ -40,13 +47,38 @@ public class SwaggerBase {
     @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
+                //可用做分组标识，但是用了这个后不能使用离线功能
                 //.groupName("User：用户信息")
+                //是否加入自己默认的参数
+                .globalOperationParameters(getParmeter())
+                //加载默认配置
                 .apiInfo(apiInfo())
                 .select()
+                //设置扫描的包（若分组可以指定到具体的包下）
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 ;
+    }
+
+    /**
+     *开 发 者：连磊
+     *开发时间：2018/9/14 15:24
+     *方 法 名：getParmeter
+     *传入参数：[]
+     *返 回 值：java.util.List<springfox.documentation.service.Parameter>
+     *描    述：设置一个默认请求标识
+     **/
+    private List<Parameter> getParmeter(){
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+        //设置一个默认的参数
+        parameterBuilder.name("CurTime").description("当前用户的登录Token").defaultValue(String.valueOf(System.currentTimeMillis()))
+                //标识出当前参数类型，和位置
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+        parameters.add(parameterBuilder.build());
+        return parameters;
     }
 
 }
